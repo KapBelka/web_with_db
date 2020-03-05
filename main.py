0beecc4982b57ba10ee1a4e4e9cf869d254eb3fc
pst <pst@mail.ru>
2020-03-05 18:05:19 +0700

    v 0.1

--- main.py
+++ main.py
@@ -1,17 +1,58 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
+from wtforms import *
+from wtforms.fields.html5 import *
+from wtforms.validators import DataRequired
+
 from data import db_session
+from data.news import News
+from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
+class RegisterForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    about = TextAreaField("Немного о себе")
+    submit = SubmitField('Войти')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-    param = {}
-    param['username'] = "Ученик Яндекс.Лицея"
-    param['title'] = 'Приветствие'
-    return render_template('index.html', **param)
+    session = db_session.create_session()
+    news = session.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
 
 
 def main():
