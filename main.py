75c1d3b365ea1b68dac2b4923ba4e9ec24606141
Belka <lep12345@mail.ru>
2020-03-20 15:46:01 +0700

    4

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from data import db_session
 from data.users import User
+from data.jobs import Jobs
+import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -30,15 +32,23 @@ def add_colonist(surname, name, age, position, speciality, address, email, hashe
     session.commit()
 
 
+def add_job(team_leader, job_desctription, work_size, collaborators, is_finished,
+            start_date=datetime.datetime.now()):
+    session = db_session.create_session()
+    job = Jobs()
+    job.team_leader = team_leader
+    job.job = job_desctription
+    job.work_size = work_size
+    job.collaborators = collaborators
+    job.start_date = start_date
+    job.is_finished = is_finished
+    session.add(job)
+    session.commit()
+
+
 def main():
     db_session.global_init("db/mars_explorer.db")
-    add_colonist("Scott", "Ridley", 21, "captain", "research engineer", "module_1",
-                 "scott_chief@mars.org", "cap")
-    add_colonist("Uir", "Endi", 23, "colonist", "builder", "module_1", "endi_col@mars.org", "enuir")
-    add_colonist("Uotni", "Mark", 19, "colonist", "main engineer", "module_1", "mark_u@mars.org",
-                 "markiu")
-    add_colonist("Bin", "Shon", 21, "colonist", "doctor", "module_1", "shon_bin@mars.org",
-                 "binbinshon")
+    add_job(1, "deployment of residential modules 1 and 2", 15, "2, 3", False)
     app.run()
 
 
