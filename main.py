214dfaf92f213dcc124e68cd04e89ce7bb728bb7
Belka <lep12345@mail.ru>
2020-03-20 22:44:49 +0700

    register

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
+from wtforms import *
+from wtforms.fields.html5 import *
+from wtforms.validators import DataRequired
+
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
@@ -6,6 +12,19 @@ import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Login/email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_again = PasswordField('Repeat password', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    age = StringField('Age', validators=[DataRequired()])
+    position = StringField('Position', validators=[DataRequired()])
+    speciality = StringField('Speciality', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 def get_all_jobs(session):
@@ -33,6 +52,35 @@ def index():
     param['jobs_list'] = jobs
     param['team_leaders'] = teamleaders
     return render_template('index.html', **param)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data,
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        return redirect('/index')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 def add_colonist(surname, name, age, position, speciality, address, email, hashed_password):
